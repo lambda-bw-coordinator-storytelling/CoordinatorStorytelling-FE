d1ef0bc0b23abb2a6032d14abcb66f961ae5f503
Dave Irwin <david@redirwin.com>
2019-06-24 13:49:46 -0700

    Created coordinator header & footer. Refactored component folders.

--- bcf/src/App.js
+++ bcf/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.scss";
 
 // import CoordinatorLogin from "./components/CoordinatorLogin";
-import CoordinatorSignup from "./components/CoordinatorSignup";
+import CoordinatorSignup from "./Containers/CoordinatorSignup";
 
 function App() {
   return (
---
+++ bcf/src/FunctionalComponents/CoordinatorHeader.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const CoordinatorHeader = () => {
+  return (
+    <>
+      <a href="">About</a>
+      <a href="">Stories</a>
+      <a href="">Partners</a>
+      <div className="coordinator-welcome-container">
+        <p>Welcome, Coordinator</p>
+        <div className="coordinator-profile-image" />
+      </div>
+    </>
+  );
+};
+
+export default CoordinatorHeader;
