fcf43d140f043f3f45aa9aa25e2c120f624a6f1c
Dave Irwin <david@redirwin.com>
2019-06-24 12:58:46 -0700

    Created basic login and signup forms.

--- bcf/src/App.js
+++ bcf/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.scss";
 
-import CoordinatorLogin from "./components/CoordinatorLogin";
+// import CoordinatorLogin from "./components/CoordinatorLogin";
+import CoordinatorSignup from "./components/CoordinatorSignup";
 
 function App() {
   return (
     <div className="App">
-      <CoordinatorLogin />
+      <CoordinatorSignup />
     </div>
   );
 }
--- bcf/src/components/CoordinatorSignup.js
+++ bcf/src/components/CoordinatorSignup.js
@@ -1,16 +1,72 @@
 import React, { Component } from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  /* ... */
-`;
 
 class CoordinatorSignup extends Component {
+  state = {
+    credentials: {
+      firstName: "",
+      lastName: "",
+      username: "",
+      password: ""
+    }
+  };
+
+  handleChanges = e => {
+    console.log(e.target.value);
+    this.setState({
+      credentials: {
+        ...this.state.credientials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  signUp = e => {
+    e.preventDefault();
+    console.log("In create account function.");
+
+    // this.props.signUp(
+    //   this.state.credentials.then(() => {
+    //     this.props.history.push("/protected");
+    //   })
+    // );
+  };
+
   render() {
     return (
-      <Container>
-        <h2>Create an Account</h2>
-      </Container>
+      <div className="coordinator-signup-page">
+        <form onSubmit={this.signUp}>
+          <h2>Create an Account</h2>
+          <p>First Name</p>
+          <input
+            type="text"
+            name="firstName"
+            value={this.state.credentials.firstName}
+            onChange={this.handleChanges}
+          />
+          <p>Last Name</p>
+          <input
+            type="text"
+            name="lastName"
+            value={this.state.credentials.lastName}
+            onChange={this.handleChanges}
+          />
+          <p>Username</p>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChanges}
+          />
+          <p>Password</p>
+          <input
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChanges}
+          />
+          <button>Sign In</button>
+        </form>
+      </div>
     );
   }
 }
