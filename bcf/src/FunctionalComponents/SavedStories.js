c144bab53838bbb229058e6ef26fe48ac513cccf
Dave Irwin <david@redirwin.com>
2019-06-24 19:38:19 -0700

    Renamed component.

--- bcf/src/FunctionalComponents/SavedStories.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SavedStories = () => {
-  return <h2>Saved Stories</h2>;
-};
-
-export default SavedStories;
---
+++ bcf/src/FunctionalComponents/YourStories.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const YourStories = () => {
+  return <h2>Your Stories</h2>;
+};
+
+export default YourStories;
