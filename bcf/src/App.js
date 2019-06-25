2080d65a7b7e5fbd1c71566bd16512b87e31c156
Dave Irwin <david@redirwin.com>
2019-06-25 08:01:38 -0700

    Starting user authentication work.

--- bcf/src/App.js
+++ bcf/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.scss";
 
-// import CoordinatorLogin from "./Containers/CoordinatorLogin";
+import CoordinatorLogin from "./Containers/CoordinatorLogin";
 // import CoordinatorSignup from "./Containers/CoordinatorSignup";
 // import MainStories from "./Containers/MainStories";
-import CoordinatorProfile from "./Containers/CoordinatorProfile";
+// import CoordinatorProfile from "./Containers/CoordinatorProfile";
 
 function App() {
   return (
     <div className="App">
-      <CoordinatorProfile />
+      <CoordinatorLogin />
     </div>
   );
 }
