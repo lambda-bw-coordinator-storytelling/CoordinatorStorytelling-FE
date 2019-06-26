afa243f36c6d4b2bef678b91cf0a44dc4b4c636b
Dave Irwin <david@redirwin.com>
2019-06-26 00:55:04 -0700

    Uploaded images from design files.

--- src/FunctionalComponents/ViewStory.js
+++ src/FunctionalComponents/ViewStory.js
@@ -1,53 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-import MainHeader from "./MainHeader";
+import NavBar from "./NavBar";
 import Footer from "./Footer";
 
-const Story = () => {
+const ViewStory = props => {
+  const { story } = props;
+
   return (
     <>
-      <MainHeader />
-      <div className="back-container">
+      <NavBar />
+      <div className="click-back-container">
         <div className="back-arrow" />
         <a href="">Back to Stories</a>
       </div>
 
       <div className="story-container">
         <div className="title-line">
-          <h2>Grad Gets Her Big Win</h2>
-          <p>May 17, 2019</p>
+          <h2>{story.title}</h2>
+          <p>{story.date}</p>
         </div>
-        <p className="article-country">Madagascar</p>
-        <div className="story">
-          <p>
-            Ullamco consectetur est duis officia esse elit ullamco. Occaecat
-            consequat proident adipisicing do eu consequat aliqua velit duis
-            sunt. Mollit cupidatat consequat ipsum eu ea incididunt laborum sint
-            commodo dolore sunt qui reprehenderit.
-          </p>
-          <p>
-            Ullamco consectetur est duis officia esse elit ullamco. Occaecat
-            consequat proident adipisicing do eu consequat aliqua velit duis
-            sunt. Mollit cupidatat consequat ipsum eu ea incididunt laborum sint
-            commodo dolore sunt qui reprehenderit.
-          </p>
-          <p>
-            Ullamco consectetur est duis officia esse elit ullamco. Occaecat
-            consequat proident adipisicing do eu consequat aliqua velit duis
-            sunt. Mollit cupidatat consequat ipsum eu ea incididunt laborum sint
-            commodo dolore sunt qui reprehenderit.
-          </p>
-          <p>
-            Ullamco consectetur est duis officia esse elit ullamco. Occaecat
-            consequat proident adipisicing do eu consequat aliqua velit duis
-            sunt. Mollit cupidatat consequat ipsum eu ea incididunt laborum sint
-            commodo dolore sunt qui reprehenderit.
-          </p>
-        </div>
+        <p className="article-country">{story.country}</p>
+        <div className="story">{story.content}</div>
       </div>
       <Footer />
     </>
   );
 };
 
-export default Story;
+export default ViewStory;
