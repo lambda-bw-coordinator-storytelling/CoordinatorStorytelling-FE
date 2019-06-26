e785ec6d4163b5574937a4436ad7b08ba2049b76
Dave Irwin <david@redirwin.com>
2019-06-25 20:27:36 -0700

    Private routes not working, will revisit. Sucessfully pulling all stories from db, assigning to state, and passing down.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-
+import { BrowserRouter, Route } from "react-router-dom";
+import { PrivateRoute } from "./Helpers/PrivateRoute";
 import "./App.scss";
-import { ProtectedRoute } from "./Helpers/ProtectedRoute";
+
 import MainHeader from "./FunctionalComponents/MainHeader";
 import Footer from "./FunctionalComponents/Footer";
 import MainStories from "./Containers/MainStories";
 import CoordinatorLogin from "./Containers/CoordinatorLogin";
 import CoordinatorSignup from "./Containers/CoordinatorSignup";
 import CoordinatorHome from "./Containers/CoordinatorHome";
-import CoordinatorProfile from "./Containers/CoordinatorProfile";
-import StoryAdd from "./Containers/StoryAdd";
-import StoryEdit from "./Containers/StoryEdit";
+// import CoordinatorProfile from "./Containers/CoordinatorProfile";
+// import StoryAdd from "./Containers/StoryAdd";
+// import StoryEdit from "./Containers/StoryEdit";
 
 function App() {
   return (
@@ -21,18 +21,7 @@ function App() {
         <Route exact path="/" component={MainStories} />
         <Route path="/login" component={CoordinatorLogin} />
         <Route path="/signup" component={CoordinatorSignup} />
-        <ProtectedRoute exact path="/protected" component={CoordinatorHome} />
-        <ProtectedRoute exact path="/protected/addstory" component={StoryAdd} />
-        <ProtectedRoute
-          exact
-          path="/protected/editstory"
-          component={StoryEdit}
-        />
-        <ProtectedRoute
-          exact
-          path="/protected/profile"
-          component={CoordinatorProfile}
-        />
+        <PrivateRoute exact path="/protected" component={CoordinatorHome} />
 
         <Footer />
       </div>
--- src/Containers/MainStories.js
+++ src/Containers/MainStories.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import RecentStories from "../FunctionalComponents/RecentStories";
 import TodaysStories from "../FunctionalComponents/TodaysStories";
@@ -7,7 +8,8 @@ import StoriesList from "../FunctionalComponents/StoriesList";
 
 class MainStories extends Component {
   state = {
-    countryFilter: "all"
+    countryFilter: "all",
+    stories: []
   };
 
   setFilter = e => {
@@ -16,6 +18,21 @@ class MainStories extends Component {
       countryFilter: newCountry
     });
   };
+
+  componentDidMount() {
+    axios
+      .get("http://coordinator-storytelling.herokuapp.com/stories/all")
+
+      .then(res => {
+        this.setState = {
+          stories: res.data
+        };
+      })
+
+      .catch(function() {
+        console.log("There was an error: ");
+      });
+  }
 
   render() {
     return (
@@ -36,7 +53,10 @@ class MainStories extends Component {
           </form>
         </div>
         <h2>Explore Stories</h2>
-        <StoriesList filterBy={this.state.countryFilter} />
+        <StoriesList
+          filterBy={this.state.countryFilter}
+          stories={this.state.stories}
+        />
         {/* pass country as filterBy */}
       </div>
     );
