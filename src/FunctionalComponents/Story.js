500f43621887382c3130638ec30948beaf654c99
Dave Irwin <david@redirwin.com>
2019-06-26 08:44:53 -0700

    Sucessfully fetching and displaying post with dynamic id.

--- src/FunctionalComponents/Story.js
+++ src/FunctionalComponents/Story.js
@@ -17,10 +17,11 @@ const Story = props => {
         <div className="media">
           <img src={story.url} alt={story.title} />
         </div>
+
         <h3 className="title">{story.title}</h3>
-        <div className="country">{story.country}</div>
-        <p className="description">{story.description}</p>
       </Link>
+      <div className="country">{story.country}</div>
+      <p className="description">{story.description}</p>
     </>
   );
 };
--- src/FunctionalComponents/ViewStory.js
+++ src/FunctionalComponents/ViewStory.js
@@ -1,43 +1,49 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 
 class ViewStory extends Component {
   state = {
-    id: null
+    post: null
   };
 
   componentDidMount() {
-    console.log("This.prop is: ", this.props);
-
     let storyid = this.props.match.params.id;
 
-    // const { story } = this.props.location.state;
-    // this.setState(() => ({ story: story.data }));
-    // console.log(this.state);
-    // componentDidMount() {
-    //   axios
-    //     .get("http://coordinator-storytelling.herokuapp.com/stories/all")
-    //     .then(res => {
-    //       console.log(res);
-    //       this.setState(() => ({ stories: res.data }));
-    //     })
-    //     .catch(function() {
-    //       console.log("There was an error: ");
-    //     });
-    // }
+    axios
+      .get(`http://coordinator-storytelling.herokuapp.com/stories/${storyid}`)
+      .then(res => {
+        this.setState({
+          post: res.data
+        });
+        console.log(this.state.post.title);
+      })
+      .catch(function() {
+        console.log("There was an error: ");
+      });
+
+    this.setState({
+      id: storyid
+    });
   }
 
   render() {
+    const post = this.state.post ? (
+      <h2>{this.state.post.title}</h2>
+    ) : (
+      <p>Loading post...</p>
+    );
+
     return (
       <>
         <NavBar />
         <div className="click-back-container">
           <Link to=""> &#129120; Back to Stories</Link>
         </div>
-        <h2>Story Container Here</h2>
+        {post}
 
         <Footer />
       </>
